a19e98b733023123268ff9c2cb7b7ad5cbcc9c16
Theo <theo@example.com>
2015-03-28 18:22:26 +0300

    Add restler with usage example

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var rest = require('restler');
+
 module.exports = {
 	connect: function() {
-		return };
+		return 
+	},
+
+	creditbalance: function () {
+		rest.get('http://45.55.165.169:8088/v1/creditbalance?&pubkey=wallet').on('complete', function(data) {
+			console.log(data); // auto convert to object
+		});
+	}
+};
