5f88af78dfedde0d713ff7f67c236dc463230b5a
Theo <theo@example.com>
2015-03-29 21:27:56 +0400

    Change example.js

--- examples/example.js
+++ examples/example.js
@@ -6,5 +6,3 @@ var pubkey = 'wallet';
 // factomjs.creditbalance(pubkey);
 // factomjs.dblocksbyrange(0, 0);
 // factomjs.buycredit(pubkey, 100);
-factomjs.submitentry('f4f614fd9b59fe26827137937d401e2b82125c4eb48f966e6e8d30f187184cb0', ['mydata'], 'Yo! Yo! Yo! From the wrapper');
-// factomjs.entry('536f6d65206461746120746f207361766520696e20666163746f6d');
